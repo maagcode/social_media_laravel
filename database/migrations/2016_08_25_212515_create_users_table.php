4730f54dc3fc04209e5f137aee56b2426798111d
vagrant <vagrant@homestead>
2016-08-25 21:45:31 +0000

    Added Users migration

--- database/migrations/2016_08_25_212515_create_users_table.php
+++ database/migrations/2016_08_25_212515_create_users_table.php
@@ -12,12 +12,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function(Blueprint $table)
+        {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email');
+            $table->string('location')->nullable();
+            $table->string('remember_token')->nullable();
             $table->timestamps();
         });
     }
